9445eeb152b6776e77a597c13c38d181f8c40093
ElvisMw <elvismwanthi@gmail.com>
2023-06-29 07:02:46 -0700

    correction on betty style in the files.

--- get_size_1.c
+++ get_size_1.c
@@ -2,26 +2,26 @@
 
 /**
  * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
+ * @format: represents formatted string in which to print the arguments
  * @e_j: List of arguments to be printed.
  *
- * Return: Precision.
+ * Return: Precision upon completion.
  */
 int get_size(const char *format, int *e_j)
 {
-    int curr_e_j = *e_j + 1;
-    int size = 0;
+	int curr_e_j = *e_j + 1;
+	int size = 0;
 
-    if (format[curr_e_j] == 'l')
-        size = S_LONG;
-    else if (format[curr_e_j] == 'h')
-        size = S_SHORT;
+	if (format[curr_e_j] == 'l')
+		size = S_LONG;
+	else if (format[curr_e_j] == 'h')
+		size = S_SHORT;
 
-    if (size == 0)
-        *e_j = curr_e_j - 1;
-    else
-        *e_j = curr_e_j;
+	if (size == 0)
+		*e_j = curr_e_j - 1;
+	else
+		*e_j = curr_e_j;
 
-    return (size);
+	return (size);
 }
 
